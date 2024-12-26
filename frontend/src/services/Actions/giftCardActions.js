a9c3eba7652efe1d3ba9ac884793751838b9406c
Shesha2002 <gundshesha2002@gmail.com>
2024-12-26 18:51:54 +0530

    created reducers for listing giftcards

--- frontend/src/services/Actions/giftCardActions.js
+++ frontend/src/services/Actions/giftCardActions.js
@@ -1,6 +1,8 @@
 // actions/giftCardActions.js
 import axios from "axios";
-import { CREATE_GIFTCARD_REQUEST, CREATE_GIFTCARD_SUCCESS, CREATE_GIFTCARD_FAIL } from "../Constants/giftCardConstants";
+import { CREATE_GIFTCARD_REQUEST, CREATE_GIFTCARD_SUCCESS, CREATE_GIFTCARD_FAIL, LIST_GIFTCARDS_REQUEST,
+  LIST_GIFTCARDS_SUCCESS,
+  LIST_GIFTCARDS_FAIL, } from "../Constants/giftCardConstants";
 
 export const createGiftCard = (giftCardData) => async (dispatch) => {
   try {
@@ -20,6 +22,27 @@ export const createGiftCard = (giftCardData) => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: CREATE_GIFTCARD_FAIL,
+      payload: error.response && error.response.data.message ? error.response.data.message : error.message,
+    });
+  }
+};
+
+
+// Action to fetch all gift cards
+export const listGiftCards = () => async (dispatch) => {
+  try {
+    dispatch({ type: LIST_GIFTCARDS_REQUEST }); // Dispatch request action
+
+    // Fetch gift cards from the backend
+    const { data } = await axios.get("/api/v1/admin/list"); // Adjust endpoint as needed
+
+    dispatch({
+      type: LIST_GIFTCARDS_SUCCESS,
+      payload: data, // Send the fetched gift cards as payload
+    });
+  } catch (error) {
+    dispatch({
+      type: LIST_GIFTCARDS_FAIL,
       payload: error.response && error.response.data.message ? error.response.data.message : error.message,
     });
   }
--- frontend/src/services/Reducers/giftCardsReducer.js
+++ frontend/src/services/Reducers/giftCardsReducer.js
@@ -4,7 +4,11 @@ import {
   CREATE_GIFTCARD_SUCCESS,
   CREATE_GIFTCARD_FAIL,
   CREATE_GIFTCARD_RESET,
+  LIST_GIFTCARDS_REQUEST,
+  LIST_GIFTCARDS_SUCCESS,
+  LIST_GIFTCARDS_FAIL,
 } from "../Constants/giftCardConstants";
+
 
 export const giftCardCreateReducer = (state = {}, action) => {
   switch (action.type) {
@@ -16,6 +20,40 @@ export const giftCardCreateReducer = (state = {}, action) => {
       return { loading: false, error: action.payload };
     case CREATE_GIFTCARD_RESET:
       return {};
+    default:
+      return state;
+  }
+};
+
+const initialState = {
+  loading: false,
+  giftCards: [],
+  error: null,
+};
+
+// Reducer for listing gift cards
+export const giftCardListReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case LIST_GIFTCARDS_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+
+    case LIST_GIFTCARDS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        giftCards: action.payload,
+      };
+
+    case LIST_GIFTCARDS_FAIL:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+
     default:
       return state;
   }
