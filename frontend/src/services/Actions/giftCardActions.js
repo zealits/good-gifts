8639e7a8466162f6c3dd8f0eac56bf1dea073e00
Aniket Arjun Khillare <aniketkhillare17@gmail.com>
2024-12-26 19:37:59 +0530

    complete listing, updation and delete of giftcard

--- frontend/src/services/Actions/giftCardActions.js
+++ frontend/src/services/Actions/giftCardActions.js
@@ -1,8 +1,19 @@
 // actions/giftCardActions.js
 import axios from "axios";
-import { CREATE_GIFTCARD_REQUEST, CREATE_GIFTCARD_SUCCESS, CREATE_GIFTCARD_FAIL, LIST_GIFTCARDS_REQUEST,
+import {
+  CREATE_GIFTCARD_REQUEST,
+  CREATE_GIFTCARD_SUCCESS,
+  CREATE_GIFTCARD_FAIL,
+  LIST_GIFTCARDS_REQUEST,
   LIST_GIFTCARDS_SUCCESS,
-  LIST_GIFTCARDS_FAIL, } from "../Constants/giftCardConstants";
+  LIST_GIFTCARDS_FAIL,
+  UPDATE_GIFTCARD_REQUEST,
+  UPDATE_GIFTCARD_SUCCESS,
+  UPDATE_GIFTCARD_FAIL,
+  DELETE_GIFTCARD_REQUEST,
+  DELETE_GIFTCARD_SUCCESS,
+  DELETE_GIFTCARD_FAIL,
+} from "../Constants/giftCardConstants";
 
 export const createGiftCard = (giftCardData) => async (dispatch) => {
   try {
@@ -27,7 +38,6 @@ export const createGiftCard = (giftCardData) => async (dispatch) => {
   }
 };
 
-
 // Action to fetch all gift cards
 export const listGiftCards = () => async (dispatch) => {
   try {
@@ -43,6 +53,51 @@ export const listGiftCards = () => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: LIST_GIFTCARDS_FAIL,
+      payload: error.response && error.response.data.message ? error.response.data.message : error.message,
+    });
+  }
+};
+
+// Update Gift Card Action
+export const updateGiftCard = (id, updatedData) => async (dispatch) => {
+  try {
+    dispatch({ type: UPDATE_GIFTCARD_REQUEST });
+
+    // Make a PUT request to your backend
+    const { data } = await axios.put(`/api/v1/admin/update/${id}`, updatedData, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    dispatch({
+      type: UPDATE_GIFTCARD_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: UPDATE_GIFTCARD_FAIL,
+      payload: error.response && error.response.data.message ? error.response.data.message : error.message,
+    });
+  }
+};
+
+// Delete Gift Card Action
+export const deleteGiftCard = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: DELETE_GIFTCARD_REQUEST });
+    console.log(id);
+
+    // Make a DELETE request to your backend
+    await axios.delete(`/api/v1/admin/remove/${id}`);
+
+    dispatch({
+      type: DELETE_GIFTCARD_SUCCESS,
+      payload: id, // Optionally, return the deleted card's ID for frontend updates
+    });
+  } catch (error) {
+    dispatch({
+      type: DELETE_GIFTCARD_FAIL,
       payload: error.response && error.response.data.message ? error.response.data.message : error.message,
     });
   }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,12 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { giftCardCreateReducer } from "./services/Reducers/giftCardsReducer";
+import {
+  giftCardCreateReducer,
+  giftCardListReducer,
+  giftCardUpdateReducer,
+  giftCardDeleteReducer,
+} from "./services/Reducers/giftCardsReducer";
 import authReducer from "./services/Reducers/authReducer.js";
 import locationReducer from "./services/Reducers/locationSlice";
 
 // Configure the store
 const store = configureStore({
   reducer: {
-    giftCard: giftCardCreateReducer, // Add reducers here
+    giftCardCreate: giftCardCreateReducer, // Rename to indicate its purpose more clearly
+    giftCardList: giftCardListReducer, // Added giftCardListReducer here
+    giftCardUpdate: giftCardUpdateReducer,
+    giftCardDelete: giftCardDeleteReducer,
     auth: authReducer,
     location: locationReducer,
   },
